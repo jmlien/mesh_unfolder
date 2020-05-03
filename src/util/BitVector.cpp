deda18fab0c990f112305cbf08efa42981abe18c
Jyh-Ming Lien <jmlien@cs.gmu.edu>
2020-05-03 10:46:46 -0400

    add bit vector

---
+++ src/util/BitVector.cpp
@@ -0,0 +1,73 @@
+#include "BitVector.h"
+#include <cmath>
+#include <iostream>
+
+namespace masc {
+	namespace unfolding {
+
+		const int BitVector::_S = sizeof(int) * 8; //number of bits for int
+
+		BitVector::BitVector(const vector<bool>& values)
+			:_N((int)values.size())
+		{
+			data = vector<unsigned int>((int)ceil(_N*1.0f / _S), 0);
+
+			for (int i = 0; i < _N; i++) {
+				//cout<<"trying i="<<i<<endl;
+				if (values[i]) on(i);
+				// if((*this)[129]){
+				//   cout<<"129 is on why? "<<i<<endl;
+				//   exit(1);
+				// }
+			}
+		}
+
+		BitVector::BitVector(int size)
+			:_N(size)
+		{
+			data = vector<unsigned int>((int)ceil(_N*1.0f / _S), 0);
+		}
+
+		void BitVector::on(int i) {
+			assert(i < _N);
+			data[i / _S] |= (1 << (i%_S));
+		}
+
+		void BitVector::off(int i) {
+			assert(i < _N);
+			data[i / _S] &= ~(1 << (i%_S));
+		}
+
+		bool BitVector::operator[](int i) const {
+			assert(i < _N);
+			return data[i / _S] & (1 << (i%_S));
+		}
+
+		bool BitVector::operator<(const BitVector& other) const {
+			int size = data.size();
+			for (int i = size - 1; i >= 0; i--) {
+				if (data[i] != other.data[i]) return data[i] < other.data[i];
+			}
+			return false; //they are the same
+		}
+
+		ostream& operator<<(ostream& out, const BitVector& v) {
+			out << "(";
+			for (int i = 0; i < v._N; i++) out << v[i] << ((i < v._N - 1) ? "," : ")");
+			return out;
+		}
+
+		bool BitVector::operator==(const BitVector& other) const {
+			if (other._N != _N) return false;
+			int size = data.size();
+			for (int i = 0; i < size; i++) if (data[i] != other.data[i]) return false;
+			return true;
+		}
+
+		void BitVector::tovector(vector<bool>& values) const {
+			values.resize(_N); //ensure that there is enough space
+			for (int i = 0; i < _N; i++) values[i] = (*this)[i];
+		}
+
+    }
+}
---
+++ src/util/BitVector.h
@@ -0,0 +1,45 @@
+#include <vector>
+
+namespace masc {
+	namespace unfolding {
+
+		using namespace std;
+
+		//a long stream of bits
+		class BitVector {
+		public:
+
+			BitVector(const vector<bool>& values);
+			BitVector(int size);//initialize all to 0s
+
+			int size() const { return _N; }
+			void on(int i); //turn on i-th bit
+			void off(int i); //turn off i-th bit
+
+			//operator overloading
+			bool operator[](int i) const;
+			bool operator<(const BitVector& other) const; //comparator
+			friend ostream& operator<<(ostream& out, const BitVector& v);
+			bool operator==(const BitVector& other) const;
+
+			//convert BitVector to a vector of bool
+			void tovector(vector<bool>& values) const;
+
+		protected:
+			vector<unsigned int> data;
+			const static int _S; //this is sizeof(unsigned int)
+			int _N; //number of bits stored
+			friend struct hash_BitVector;
+		};
+
+		struct hash_BitVector {
+			//for hashing
+			size_t operator()(const BitVector &v) const
+			{
+				size_t code = 0x2D2816FE;
+				for (int i : v.data) return code * 31 + i; //hash<int>()(i);
+				return code;
+			}
+		};
+    }
+}
